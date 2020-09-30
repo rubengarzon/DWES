9f0769cbced357683fe3b5694512deb67b0828f4
ruben <rubengarzonzafra@gmail.com>
2020-09-30 19:22:38 +0200

    ejercicio 15

--- tema1/ejercicio15/index.php
+++ tema1/ejercicio15/index.php
@@ -13,40 +13,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
        
-<table>
-
-<tr>
-
-  <td>Celda 1</td>
-
-  <td>Celda 2</td>
-
-  <td>Celda 3</td>
-
-</tr>
-
-<tr>
-
-  <td>Celda 4</td>
-
-  <td>Celda 5</td>
-
-  <td>Celda 6</td>
-
-</tr>
-
-</table>
-
 
     <?php
         echo "<table>";
 
             for ($i=1; $i < 11; $i++) {
                     echo "<tr>";
-                        echo "tabla de multiplicar del " . $i;
                         for ($j=1; $j < 11; $j++) {
                             echo "<td>"; 
                             echo $i * $j;
