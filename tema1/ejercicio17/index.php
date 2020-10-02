cb408e8c4be22775ad6be0450da0e07ae68fefc2
ruben <rubengarzonzafra@gmail.com>
2020-10-02 09:22:41 +0200

    ejercicio 17

--- tema1/ejercicio17/index.php
+++ tema1/ejercicio17/index.php
@@ -18,7 +18,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ejercicio 17</title>
 </head>
 <body>
        
